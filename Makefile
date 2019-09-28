0f3fbf5f426a8154e49c16b7f0ec6cc5a3473044
Hemlocked <phantomofrotten@gmail.com>
2019-09-28 19:08:06 +0900

    create init make command

--- Makefile
+++ Makefile
@@ -5,3 +5,6 @@ run: src/main.rs
 
 save: src/main.rs
 	cp src/main.rs archive/${ARG}.rs
+
+init: src/main.rs
+	echo 'fn main() {\n \tprintln!("hello world");\n}' > src/main.rs
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-extern crate regex;
-
-use regex::Regex;
-
 fn main() {
-    let re = Regex::new(r"^[0-9]+$").unwrap();
-    println!("{}", re.is_match("2014-01-01"));
-    println!("{}", re.is_match("2014"));
-    println!("{}", re.is_match("h32"));
-    println!("{}", re.is_match("h4"));
-    println!("{}", re.is_match("32o"));
-    println!("{}", re.is_match("0333l"));
+ 	println!("hello world");
 }
