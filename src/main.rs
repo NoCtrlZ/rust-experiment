7e2c3698da704edc37dbaddc425b2b8b99012987
Hemlocked <phantomofrotten@gmail.com>
2019-10-20 18:07:10 +0900

    get the data from post method

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,24 @@ fn root_api() -> &'static str {
     handler()
 }
 
+#[post("/test")]
+fn test_api() -> String {
+    greet("shinsaku".to_string())
+}
+
+#[post("/check", data = "<task>")]
+fn check_api(task: String) -> String {
+    greet(task)
+}
+
 fn handler() -> &'static str {
     "Hello, world!"
 }
 
+fn greet(name: String) -> String {
+	format!("Hi {}", name)
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![root_api]).launch();
+    rocket::ignite().mount("/", routes![root_api, test_api, check_api]).launch();
 }
