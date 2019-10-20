7c772185429f227b2fec8d0b0a9f1a8c4f1be6da
Hemlocked <phantomofrotten@gmail.com>
2019-10-20 17:32:50 +0900

    create the simple api server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[post("/api")]
+fn root_api() -> &'static str {
+    handler()
+}
+
+fn handler() -> &'static str {
+    "Hello, world!"
+}
+
 fn main() {
- 	println!("hello world");
+    rocket::ignite().mount("/", routes![root_api]).launch();
 }
