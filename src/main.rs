a62e04c9560bf5985fd72c9bce657f6b321ee580
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-14 12:28:58 +0900

    create struct array

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#[derive(Debug)]
+pub struct Company {
+    pub shops: Vec<Shop>
+}
+
+#[derive(Debug)]
+pub struct Shop {
+	id: u32,
+	name: String,
+	amount: u32
+}
+
 fn main() {
- 	println!("hello world");
+ 	let mut company = Company {
+		shops: Vec::new()
+	};
+	let shop = Shop {
+		id: 8,
+		name: "hello".to_string(),
+		amount: 5
+	};
+	company.shops.push(shop);
+	println!("{:?}", company);
 }
